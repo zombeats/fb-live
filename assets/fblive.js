4aa0f541dc12ed67f932086c3d371d22ef68be65
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-12-06 22:57:26 +0100

    update

--- assets/fblive.js
+++ assets/fblive.js
@@ -1,10 +1,11 @@
-$(function() {
+/* global $ */
+$(function () {
   'use strict'
   var ACCESS_TOKEN = '1795140000768189|uqasdzez1CahupWwTL5SF1NQ2eA'
-  var POST_ID = '1137879162976857'; // PASTE HERE YOUR POST ID
-  var REFRESH_TIME_ON_SECONDS = 1;
-  var DEFAULT_COUNT = 0;
-  var IS_PRODUCTION = false;
+  var POST_ID = '1137879162976857'
+  var REFRESH_TIME_ON_SECONDS = 1
+  var DEFAULT_COUNT = 0
+  var IS_PRODUCTION = false
 
   var REACTIONS = [
     'LIKE',
@@ -13,20 +14,21 @@ $(function() {
     'HAHA',
     'SAD',
     'ANGRY'
-  ].map(function(reaction) {
-    var code = 'reactions_' + reaction.toLowerCase();
+  ].map(function (reaction) {
+    var code = 'reactions_' + reaction.toLowerCase()
     var select = 'reactions.type('
     select += reaction
     select += ').limit(0).summary(total_count).as('
     select += code
     select += ')'
     return select
-  }).join(',');
+  }).join(',')
 
   var URL =
     'https://graph.facebook.com/v2.8/?ids=' + POST_ID +
     '&fields=' + REACTIONS +
-    '&access_token=' + ACCESS_TOKEN;
+    '&access_token=' + ACCESS_TOKEN +
+    '&callback=?' // JSONP
 
   var COUNTERS = [
     { el: $('counter1'), name: 'reactions_like' },
@@ -37,16 +39,16 @@ $(function() {
     { el: $('counter6'), name: 'reactions_wow' }
   ]
 
-  function refreshCounts() {
-    $.getJSON(URL, function(res) {
-      COUNTERS.forEach(function(counter, index) {
+  function refreshCounts () {
+    $.getJSON(URL, function (res) {
+      COUNTERS.forEach(function (counter, index) {
         var counterValue = res[POST_ID][counter.name].summary.total_count
-        v1.text(DEFAULT_COUNT + counterValue)
+        counter.el.text(DEFAULT_COUNT + counterValue)
       })
-    });
+    })
   }
 
   refreshCounts()
 
-  if (IS_PRODUCTION) setInterval(refreshCounts, REFRESH_TIME_ON_SECONDS * 1000);
+  if (IS_PRODUCTION) setInterval(refreshCounts, REFRESH_TIME_ON_SECONDS * 1000)
 })
