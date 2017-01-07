ceab6724e61d3141d7ad55ea5885ae0debc19a21
Antonio Rodríguez <musantrobeats+dev@gmail.com>
2017-01-07 14:20:50 +0100

    new design & best beatbox solo video 2016

--- assets/fblive.js
+++ assets/fblive.js
@@ -2,7 +2,7 @@
 $(function () {
   'use strict'
   var ACCESS_TOKEN = '1795140000768189|uqasdzez1CahupWwTL5SF1NQ2eA'
-  var POST_ID = '1164125213685585'
+  var POST_ID = '1166181600146613'
   var REFRESH_TIME_ON_SECONDS = 1
   var DEFAULT_COUNT = 0
   var IS_PRODUCTION = true
@@ -31,23 +31,33 @@ $(function () {
     '&callback=?' // JSONP
 
   var COUNTERS = [
-    { el: $('.counter-1'), name: 'reactions_angry' },
-    { el: $('.counter-2'), name: 'reactions_haha' },
-    { el: $('.counter-3'), name: 'reactions_like' },
-    { el: $('.counter-4'), name: 'reactions_love' },
-    { el: $('.counter-5'), name: 'reactions_sad' },
-    { el: $('.counter-6'), name: 'reactions_wow' }
+    { el: $('.counter-1'), name: 'reactions_angry', progress: $('.bar-1') },
+    { el: $('.counter-2'), name: 'reactions_haha', progress: $('.bar-2') },
+    { el: $('.counter-3'), name: 'reactions_like', progress: $('.bar-3') },
+    { el: $('.counter-4'), name: 'reactions_love', progress: $('.bar-4') },
+    { el: $('.counter-5'), name: 'reactions_sad', progress: $('.bar-5') },
+    { el: $('.counter-6'), name: 'reactions_wow', progress: $('.bar-6') }
   ]
+
 
   function refreshCounts () {
     $.getJSON(URL, function (res) {
       res = res[POST_ID]
+      var totalCounts = 0;
       COUNTERS.forEach(function (counter, index) {
-        var counterValue = res[counter.name].summary.total_count
+        var counterValue = Number(res[counter.name].summary.total_count);
+        totalCounts = totalCounts + Number(counterValue);
         counter.el.text(DEFAULT_COUNT + counterValue)
+      })
+      COUNTERS.forEach(function (counter, index) {
+        var counterValue = Number(res[counter.name].summary.total_count);
+        const percent = (counterValue/totalCounts)*100
+        if(counter.progress[0]){
+          counter.progress[0].style.width = `${percent}%`;
+        }
       })
     })
   }
 
-  if (IS_PRODUCTION) setInterval(refreshCounts, REFRESH_TIME_ON_SECONDS * 500)
+  if (IS_PRODUCTION) setInterval(refreshCounts, REFRESH_TIME_ON_SECONDS * 1000)
 })
